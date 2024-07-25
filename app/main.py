9a8b6afa92b50791fe8e317c802f91e112553637
Vlada <vlada.belayy@mail.com>
2024-07-25 23:28:53 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 class Car:
     def __init__(
-        self, comfort_class: int, clean_mark: int, brand: str
+        self,
+        comfort_class: int,
+        clean_mark: int,
+        brand: str
     ) -> None:
         self.comfort_class = self.validate_comfort_class(comfort_class)
         self.clean_mark = self.validate_clean_mark(clean_mark)
@@ -19,33 +22,33 @@ class Car:
 
 class CarWashStation:
     def __init__(
-        self, distance_from_city_center: float, clean_power: int,
-        average_rating: float, count_of_ratings: int
+        self,
+        distance_from_city_center: float,
+        clean_power: int,
+        average_rating: float,
+        count_of_ratings: int
     ) -> None:
         self.distance_from_city_center = (self.validate_distance
                                           (distance_from_city_center))
         self.clean_power = self.validate_clean_power(clean_power)
         self.average_rating = self.validate_average_rating(average_rating)
-        self.count_of_ratings = (
-            self.validate_count_of_ratings(count_of_ratings))
+        self.count_of_ratings = (self.validate_count_of_ratings
+                                 (count_of_ratings))
 
     def validate_distance(self, distance_from_city_center: float) -> float:
         if 1.0 <= distance_from_city_center <= 10.0:
             return distance_from_city_center
-        else:
-            return 1.0
+        return 1.0
 
     def validate_clean_power(self, clean_power: int) -> int:
         if 1 <= clean_power <= 10:
             return clean_power
-        else:
-            return 1
+        return 1
 
     def validate_average_rating(self, average_rating: float) -> float:
         if 1.0 <= average_rating <= 5.0:
             return round(average_rating, 1)
-        else:
-            return 1.0
+        return 1.0
 
     def validate_count_of_ratings(self, count_of_ratings: int) -> int:
         if count_of_ratings >= 0:
@@ -61,19 +64,20 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        cost = (
+        return round(
             car.comfort_class * (self.clean_power - car.clean_mark)
-            * self.average_rating / self.distance_from_city_center
+            * self.average_rating / self.distance_from_city_center,
+            1
         )
-        return round(cost, 1)
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate: float) -> None:
-        total_rating = self.average_rating * self.count_of_ratings
-        self.count_of_ratings += 1
         self.average_rating = round(
-            (total_rating + rate) / self.count_of_ratings
-            , 1)
+            (self.average_rating * self.count_of_ratings + rate)
+            / (self.count_of_ratings + 1),
+            1
+        )
+        self.count_of_ratings += 1
